ce236b8e5d1493ee466ff8734aae83384083bb01
jinchuu1391 <62422486+jinchuu1391@users.noreply.github.com>
2020-11-06 15:09:38 +0900

    글쓰기 기능 구현

--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -2,11 +2,31 @@ import React from "react";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import Responsive from "../components/Responsive";
+import axios from "axios";
+import { useSelector } from "react-redux";
+
 const WritePage = () => {
+  const { title, body } = useSelector((state) => state.post);
+
+  const postHandler = () => {
+    axios
+      .post("http://localhost:4000/post", {
+        title: title,
+        content: body,
+        token: localStorage.getItem("token"),
+      })
+      .then((res) => {
+        // 해당 글 상세 페이지로 이동
+      })
+      .catch((err) => {
+        alert("ERROR! 관리자에게 문의하세요");
+      });
+  };
+
   return (
     <Responsive>
       <Editor></Editor>
-      <Button>글 올리기</Button>
+      <Button onClick={postHandler}>글 올리기</Button>
     </Responsive>
   );
 };
